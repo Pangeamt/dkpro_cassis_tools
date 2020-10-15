81357293f384f9437a0f4a4131c220343d363aba
LaurentBie <hloplol@gmail.com>
2020-10-15 13:51:25 +0200

    Typo in filename

--- dkpro_cassis_tools/__init__.py
+++ dkpro_cassis_tools/__init__.py
@@ -1,7 +1,7 @@
 from dkpro_cassis_tools.load_cas_from_zip_file import load_cas_from_zip_file
 from dkpro_cassis_tools.dump_cas_to_zip_file import dump_cas_to_zip_file
 from dkpro_cassis_tools.create_cas_from_sentences import create_cas_from_sentences
-from dkpro_cassis_tools.restore_cas_segmentatation_by_newline import restore_cas_segmentation_by_newline
+from dkpro_cassis_tools.restore_cas_segmentation_by_newline import restore_cas_segmentation_by_newline
 from dkpro_cassis_tools.ns import SENTENCE_NS, TOKEN_NS, NAMED_ENTITY_NS, DOCUMENT_METADATA
 
 
