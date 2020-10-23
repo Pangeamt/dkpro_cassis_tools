11134702185bc511e3e75a989e6483fb0a959518
LaurentBie <hloplol@gmail.com>
2020-10-23 12:09:11 +0200

    Remove incorrect multi-sofa support

--- dkpro_cassis_tools/restore_cas_segmentation_by_newline.py
+++ dkpro_cassis_tools/restore_cas_segmentation_by_newline.py
@@ -12,16 +12,15 @@ def restore_cas_segmentation_by_newline(cas: Cas) -> Cas:
         cas.remove_annotation(sentence)
 
     # Create new sentence annotations
-    for sofa in cas.sofas:
-        idx = 0
-        separator = re.compile('((?:\r?)\n)')
-        for item in re.compile(separator).split(sofa.sofaString):
-            start = idx
-            if re.match(separator, item):
-                idx += len(item)
-            elif len(item):
-                idx += len(item)
-                annotation = cas.typesystem.get_type(SENTENCE_NS)(begin=start, end=idx, sofa=sofa.sofaNum)
-                cas.add_annotation(annotation)
+    idx = 0
+    separator = re.compile('((?:\r?)\n)')
+    for item in re.compile(separator).split(cas.sofa_string):
+        start = idx
+        if re.match(separator, item):
+            idx += len(item)
+        elif len(item):
+            idx += len(item)
+            annotation = cas.typesystem.get_type(SENTENCE_NS)(begin=start, end=idx)
+            cas.add_annotation(annotation)
     return cas
 
