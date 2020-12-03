37ebf2e67261c84eae215cfdab6a9f55fd98b523
LaurentBie <hloplol@gmail.com>
2020-12-03 09:50:07 +0100

    Update dump_cas_to_zip_file.py

--- dkpro_cassis_tools/dump_cas_to_zip_file.py
+++ dkpro_cassis_tools/dump_cas_to_zip_file.py
@@ -1,9 +1,21 @@
+from typing import Optional
 import zipfile
 from cassis import Cas
 
 
-def dump_cas_to_zip_file(cas: Cas, zip_file, xmi_name: str = "xmi.xmi", type_system_file: str = "TypeSystem.xml"):
+def dump_cas_to_zip_file(
+        cas: Cas,
+        zip_file: str,
+        xmi_file: Optional[str] = None,
+        type_system_file: Optional[str] = None):
+
+    if xmi_file is None:
+        xmi_file = "xmi.xmi"
+
+    if type_system_file is None:
+        type_system_file = "TypeSystem.xml"
+
     zf = zipfile.ZipFile(zip_file, "w")
     zf.writestr(type_system_file, cas.typesystem.to_xml())
-    zf.writestr(xmi_name, cas.to_xmi(pretty_print=True))
+    zf.writestr(xmi_file, cas.to_xmi(pretty_print=True))
     zf.close()
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dkpro_cassis_tools",
-    version="0.0.2",
+    version="0.0.3",
     author="Laurent Bié",
     author_email="l.bie@pangeanic.com  ",
     description="Tools for dkpro cassis",
