acbd349b988e935996dcafd39141d6342ee64eca
LaurentBie <hloplol@gmail.com>
2020-12-14 10:34:04 +0100

    Bug correction

--- dkpro_cassis_tools/get_document_meta_data.py
+++ dkpro_cassis_tools/get_document_meta_data.py
@@ -4,9 +4,6 @@ from dkpro_cassis_tools.ns import DOCUMENT_METADATA
 
 def get_document_meta_data(cas: Cas):
     document_meta_data_all = cas.select(DOCUMENT_METADATA)
-    try:
-        document_meta_data = next(document_meta_data_all)
-    except StopIteration:
-        document_meta_data = None
-
-    return document_meta_data
+    if document_meta_data_all:
+        return document_meta_data_all[0]
+    return None
