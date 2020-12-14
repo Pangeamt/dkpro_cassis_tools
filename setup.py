b47e37c37ab3f5cbd0e918c5d7df67f0c60a2621
LaurentBie <hloplol@gmail.com>
2020-12-14 09:51:50 +0100

    Update package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dkpro_cassis_tools",
-    version="0.0.3",
+    version="0.0.4",
     author="Laurent Bié",
     author_email="l.bie@pangeanic.com  ",
     description="Tools for dkpro cassis",
